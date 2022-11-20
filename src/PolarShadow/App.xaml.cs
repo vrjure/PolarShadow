b5c7d10b555a907fddd8236029831f92c013ac33
vrjure v <vvryoung@outlook.com>
2022-11-20 18:26:54 +0800

    Add project files.

---
+++ src/PolarShadow/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PolarShadow;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/PolarShadow/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PolarShadow;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
