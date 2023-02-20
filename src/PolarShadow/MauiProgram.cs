e3fcd439400de856af66f6b770d93a6cbf49e39d
vrjure v <vvryoung@outlook.com>
2023-02-21 00:14:47 +0800

    layout

--- src/PolarShadow/MauiProgram.cs
+++ src/PolarShadow/MauiProgram.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PolarShadow.Core;
 using PolarShadow.Data;
+using PolarShadow.Storage;
+using System.Text.Json;
 
 namespace PolarShadow
 {
     public static class MauiProgram
     {
+        private static string _optionFileName = "source.json";
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
@@ -13,18 +18,66 @@ namespace PolarShadow
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
-
-            builder.Services.AddMauiBlazorWebView();
-
-#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
-#endif
+                }).ConfigureServices();
 
             builder.Services.AddSingleton<WeatherForecastService>();
 
-            return builder.Build();
+            return builder.Build().InitializeApp();
+        }
+
+        private static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder)
+        {
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+            builder.Services.AddAntDesign();
+
+            builder.Services.AddSingleton(CreatePolarShadowBuilder());
+            builder.Services.AddTransient(sp =>
+            {
+                var b = sp.GetRequiredService<IPolarShadowBuilder>();
+                return b.Build();
+            });
+
+            builder.Services.AddDbContextFactory<PolarShadowDbContext>(options =>
+            {
+                var path = Path.Combine(FileSystem.Current.AppDataDirectory, "polar.db3");
+                options.UseSqlite($"Data Source={path}", op => op.MigrationsAssembly(typeof(PolarShadowDbContext).Assembly.FullName));
+            });
+
+            builder.Services.AddStorageServices();
+
+            return builder;
+        }
+
+        private static IPolarShadowBuilder CreatePolarShadowBuilder()
+        {
+            var sourcePath = Path.Combine(FileSystem.AppDataDirectory, _optionFileName);
+            using var fs = new FileStream(sourcePath, FileMode.OpenOrCreate, FileAccess.Read);
+            if (fs.Length == 0)
+            {
+                var builder = new PolarShadowBuilder();
+                return builder;
+            }
+            else
+            {
+                var option = JsonSerializer.Deserialize<PolarShadowOption>(fs, JsonOption.DefaultSerializer);
+                var builder = new PolarShadowBuilder(option);
+                return builder;
+            }
+
+        }
+
+        private static MauiApp InitializeApp(this MauiApp app)
+        {
+            var dbFactory = app.Services.GetRequiredService<IDbContextFactory<PolarShadowDbContext>>();
+            using (var context = dbFactory.CreateDbContext())
+            {
+                context.Database.Migrate();
+            }
+            return app;
         }
     }
 }
