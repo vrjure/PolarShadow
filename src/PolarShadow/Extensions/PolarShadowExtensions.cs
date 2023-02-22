7fad904fb0976b1700064203ad736074880d25a9
vrjure v <vvryoung@outlook.com>
2023-02-23 00:14:08 +0800

    source

---
+++ src/PolarShadow/Extensions/PolarShadowExtensions.cs
@@ -0,0 +1,42 @@
+﻿using PolarShadow.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PolarShadow
+{
+    internal static class PolarShadowExtensions
+    {
+        private static string _optionFileName = "source.json";
+        public static async Task SaveToFileAsync(this IPolarShadowBuilder builder)
+        {
+            var sourcePath = Path.Combine(FileSystem.AppDataDirectory, _optionFileName);
+            using var fs = new FileStream(sourcePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            fs.SetLength(0);
+            var json = JsonSerializer.Serialize(builder.Option, JsonOption.DefaultSerializer);
+            var buffer = Encoding.UTF8.GetBytes(json);
+            await fs.WriteAsync(buffer);
+            await fs.FlushAsync();
+        }
+
+        public static void ReadFromFile(this IPolarShadowBuilder builder)
+        {
+            var sourcePath = Path.Combine(FileSystem.AppDataDirectory, _optionFileName);
+            using var fs = new FileStream(sourcePath, FileMode.OpenOrCreate, FileAccess.Read);
+            if (fs.Length == 0)
+            {
+                return;
+            }
+            else
+            {
+                var option = JsonSerializer.Deserialize<PolarShadowOption>(fs, JsonOption.DefaultSerializer);
+                builder.Option.AnalysisSources = option.AnalysisSources;
+                builder.Option.Sites = option.Sites;
+                builder.Option.IsChanged = true;
+            }
+        }
+    }
+}
