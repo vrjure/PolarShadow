3da74baee2405ce407fd908605d177d1c281f758
vrjure v <vvryoung@outlook.com>
2024-01-07 16:18:26 +0800

    resourcetree add ChildChhildren

--- src/PolarShadow.Resources.Models/ResourceTree.cs
+++ src/PolarShadow.Resources.Models/ResourceTree.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PolarShadow.Resources
 {
     public class ResourceTree : Resource
     {
-        public ICollection<ResourceTree> Children { get; set; }
+        private ICollection<ResourceTree> _children;
+        public ICollection<ResourceTree> Children
+        {
+            get
+            {
+                if (ChildChildren?.Count > 0 && _children?.Count > 0)
+                {
+                    var list = ChildChildren as IList<ResourceTree> ?? ChildChildren.ToList();
+                    var index = 0;
+                    foreach (var item in _children)
+                    {
+                        if (index < list.Count)
+                        {
+                            item.Children = list[index].Children;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                        index++;
+                    }
+                }
+                return _children;
+            }
+            set => _children = value;
+        }
+        public ICollection<ResourceTree> ChildChildren { get; set; }
     }
 }
